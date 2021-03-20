1a63066ab1c63987fc7f62163aef6718528c9d54
Bart De Roy <de.roy.bart@gmail.com>
2021-03-20 22:09:11 +0100

    fix json #1

--- src/blockdata.js
+++ src/blockdata.js
@@ -1,395 +1,386 @@
 {
-    motion_movesteps: {
-        blockcode: 'neem %n stappen'
+    "motion_movesteps": {
+        "blockcode": "neem %n stappen"
     },
-    motion_turnright: {
-        blockcode: 'draai @turnRight %n graden'
+    "motion_turnright": {
+        "blockcode": "draai @turnRight %n graden"
     },
-    motion_turnleft: {
-        blockcode: 'draai @turnLeft %n graden'
+    "motion_turnleft": {
+        "blockcode": "draai @turnLeft %n graden"
     },
-    motion_goto: {
-        blockcode: 'ga naar %r'
+    "motion_goto": {
+        "blockcode": "ga naar %r"
     },
-    motion_goto_menu: {
-        blockcode: ''
+    "motion_goto_menu": {
+        "blockcode": ""
     },
-    motion_gotoxy: {
-        blockcode: 'ga naar x: %n y: %n'
+    "motion_gotoxy": {
+        "blockcode": "ga naar x: %n y: %n"
     },
-    motion_glideto: {
-        blockcode: 'schuif in %n sec. naar %r'
+    "motion_glideto": {
+        "blockcode": "schuif in %n sec. naar %r"
     },
-    motion_glideto_menu: {
-        blockcode: ''
+    "motion_glideto_menu": {
+        "blockcode": ""
     },
-    motion_glidesecstoxy: {
-        blockcode: 'schuif in %n sec. naar x: %n y: %n'
+    "motion_glidesecstoxy": {
+        "blockcode": "schuif in %n sec. naar x: %n y: %n"
     },
-    motion_pointindirection: {
-        blockcode: 'richt naar %n graden'
+    "motion_pointindirection": {
+        "blockcode": "richt naar %n graden"
     },
-    motion_pointtowards: {
-        blockcode: 'richt naar %r'
+    "motion_pointtowards": {
+        "blockcode": "richt naar %r"
     },
-    motion_pointtowards_menu: {
-        blockcode: ''
+    "motion_pointtowards_menu": {
+        "blockcode": ""
     },
-    motion_changexby: {
-        blockcode: 'verander x met %n'
+    "motion_changexby": {
+        "blockcode": "verander x met %n"
     },
-    motion_setx: {
-        blockcode: 'maak x %n'
+    "motion_setx": {
+        "blockcode": "maak x %n"
     },
-    motion_changeyby: {
-        blockcode: 'verander y met %n'
+    "motion_changeyby": {
+        "blockcode": "verander y met %n"
     },
-    motion_sety: {
-        blockcode: 'maak y %n'
+    "motion_sety": {
+        "blockcode": "maak y %n"
     },
-    motion_ifonedgebounce: {
-        blockcode: 'keer om aan de rand'
+    "motion_ifonedgebounce": {
+        "blockcode": "keer om aan de rand"
     },
-    motion_setrotationstyle: {
-        blockcode: 'maak draaistijl %m'
+    "motion_setrotationstyle": {
+        "blockcode": "maak draaistijl %m"
     },
-    looks_sayforsecs: {
-        blockcode: 'zeg %s %n sec.'
+    "looks_sayforsecs": {
+        "blockcode": "zeg %s %n sec."
     },
-    looks_say: {
-        blockcode: 'zeg %s'
+    "looks_say": {
+        "blockcode": "zeg %s"
     },
-    looks_thinkforsecs: {
-        blockcode: 'denk %s %n sec.'
+    "looks_thinkforsecs": {
+        "blockcode": "denk %s %n sec."
     },
-    looks_think: {
-        blockcode: 'denk %s'
+    "looks_think": {
+        "blockcode": "denk %s"
     },
-    looks_switchcostumeto: {
-        blockcode: 'verander uiterlijk naar %r'
+    "looks_switchcostumeto": {
+        "blockcode": "verander uiterlijk naar %r"
     },
-    looks_costume: {
-        blockcode: ''
+    "looks_costume": {
+        "blockcode": ""
     },
-    looks_nextcostume: {
-        blockcode: 'volgend uiterlijk'
+    "looks_nextcostume": {
+        "blockcode": "volgend uiterlijk"
     },
-    looks_switchbackdropto: {
-        blockcode: 'verander achtergrond naar %r ::looks'
+    "looks_switchbackdropto": {
+        "blockcode": "verander achtergrond naar %r ::looks"
     },
-    looks_backdrops: {
-        blockcode: ''
+    "looks_backdrops": {
+        "blockcode": ""
     },
-    looks_nextbackdrop: {
-        blockcode: 'volgend uiterlijk'
+    "looks_nextbackdrop": {
+        "blockcode": "volgend uiterlijk"
     },
-    looks_changesizeby: {
-        blockcode: 'verander grootte met %n'
+    "looks_changesizeby": {
+        "blockcode": "verander grootte met %n"
     },
-    looks_setsizeto: {
-        blockcode: 'maak grootte %n'
+    "looks_setsizeto": {
+        "blockcode": "maak grootte %n"
     },
-    looks_changeeffectby: {
-        blockcode: 'verander %m-effect met %n ::looks'
+    "looks_changeeffectby": {
+        "blockcode": "verander %m-effect met %n ::looks"
     },
-    looks_seteffectto: {
-        blockcode: 'zet effect %m op %n ::looks'
+    "looks_seteffectto": {
+        "blockcode": "zet effect %m op %n ::looks"
     },
-    looks_cleargraphiceffects: {
-        blockcode: 'zet alle effecten uit'
+    "looks_cleargraphiceffects": {
+        "blockcode": "zet alle effecten uit"
     },
-    looks_show: {
-        blockcode: 'verschijn'
+    "looks_show": {
+        "blockcode": "verschijn"
     },
-    looks_hide: {
-        blockcode: 'verdwijn'
+    "looks_hide": {
+        "blockcode": "verdwijn"
     },
-    looks_gotofrontback: {
-        blockcode: 'ga naar laag %m ::looks'
+    "looks_gotofrontback": {
+        "blockcode": "ga naar laag %m ::looks"
     },
-    looks_goforwardbackwardlayers: {
-        blockcode: 'ga %m %n lagen'
+    "looks_goforwardbackwardlayers": {
+        "blockcode": "ga %m %n lagen"
     },
-    sound_playuntildone: {
-        blockcode: 'start geluid %r en wacht'
+    "sound_playuntildone": {
+        "blockcode": "start geluid %r en wacht"
     },
-    sound_sounds_menu: {
-        blockcode: ''
+    "sound_play": {
+        "blockcode": "start geluid %r ::sound"
     },
-    sound_play: {
-        blockcode: 'start geluid %r ::sound'
+    "sound_stopallsounds": {
+        "blockcode": "stop alle geluiden"
     },
-    sound_sounds_menu: {
-        blockcode: ''
+    "sound_changeeffectby": {
+        "blockcode": "verander %m-effect met %n ::sound"
     },
-    sound_stopallsounds: {
-        blockcode: 'stop alle geluiden'
+    "sound_seteffectto": {
+        "blockcode": "zet %m effect op %n ::sound"
     },
-    sound_changeeffectby: {
-        blockcode: 'verander %m-effect met %n ::sound'
+    "sound_cleareffects": {
+        "blockcode": "zet alle effecten uit"
     },
-    sound_seteffectto: {
-        blockcode: 'zet %m effect op %n ::sound'
+    "sound_changevolumeby": {
+        "blockcode": "verander volume met %n"
     },
-    sound_cleareffects: {
-        blockcode: 'zet alle effecten uit'
+    "sound_setvolumeto": {
+        "blockcode": "verander volume met %n"
     },
-    sound_changevolumeby: {
-        blockcode: 'verander volume met %n'
+    "event_whenflagclicked": {
+        "blockcode": "wanneer groene vlag wordt aangeklikt"
     },
-    sound_setvolumeto: {
-        blockcode: 'verander volume met %n'
+    "event_whenkeypressed": {
+        "blockcode": "wanneer %m is ingedrukt"
     },
-    event_whenflagclicked: {
-        blockcode: 'wanneer groene vlag wordt aangeklikt'
+    "event_whenthisspriteclicked": {
+        "blockcode": "wanneer op deze sprite wordt geklikt"
     },
-    event_whenkeypressed: {
-        blockcode: 'wanneer %m is ingedrukt'
+    "event_whenbackdropswitchesto": {
+        "blockcode": "wanneer achtergrond verandert naar %m"
     },
-    event_whenthisspriteclicked: {
-        blockcode: 'wanneer op deze sprite wordt geklikt'
+    "event_whengreaterthan": {
+        "blockcode": "wanneer %m > %n"
     },
-    event_whenbackdropswitchesto: {
-        blockcode: 'wanneer achtergrond verandert naar %m'
+    "event_whenbroadcastreceived": {
+        "blockcode": "wanneer ik signaal %m ontvang"
     },
-    event_whengreaterthan: {
-        blockcode: 'wanneer %m > %n'
+    "event_broadcast": {
+        "blockcode": "zend signaal %r"
     },
-    event_whenbroadcastreceived: {
-        blockcode: 'wanneer ik signaal %m ontvang'
+    "event_broadcastandwait": {
+        "blockcode": "zend signaal %r en wacht"
     },
-    event_broadcast: {
-        blockcode: 'zend signaal %r'
+    "control_wait": {
+        "blockcode": "wacht %n sec."
     },
-    event_broadcastandwait: {
-        blockcode: 'zend signaal %r en wacht'
+    "control_repeat": {
+        "blockcode": "herhaal %n {}"
     },
-    control_wait: {
-        blockcode: 'wacht %n sec.'
+    "control_forever": {
+        "blockcode": "herhaal {}"
     },
-    control_repeat: {
-        blockcode: 'herhaal %n {}'
+    "control_if": {
+        "blockcode": "als %b dan {}"
     },
-    control_forever: {
-        blockcode: 'herhaal {}'
+    "control_if_else": {
+        "blockcode": "als %b dan {} anders {}"
     },
-    control_if: {
-        blockcode: 'als %b dan {}'
+    "control_wait_until": {
+        "blockcode": "wacht tot %b"
     },
-    control_if_else: {
-        blockcode: 'als %b dan {} anders {}'
+    "control_repeat_until": {
+        "blockcode": "herhaal tot %b {}"
     },
-    control_wait_until: {
-        blockcode: 'wacht tot %b'
+    "control_stop": {
+        "blockcode": "stop %m"
     },
-    control_repeat_until: {
-        blockcode: 'herhaal tot %b {}'
+    "control_start_as_clone": {
+        "blockcode": "wanneer ik als kloon start"
     },
-    control_stop: {
-        blockcode: 'stop %m'
+    "control_create_clone_of": {
+        "blockcode": "maak een kloon van %r"
     },
-    control_start_as_clone: {
-        blockcode: 'wanneer ik als kloon start'
+    "control_create_clone_of_menu": {
+        "blockcode": ""
     },
-    control_create_clone_of: {
-        blockcode: 'maak een kloon van %r'
+    "control_delete_this_clone": {
+        "blockcode": "verwijder deze kloon"
     },
-    control_create_clone_of_menu: {
-        blockcode: ''
+    "motion_xposition": {
+        "blockcode": "x-positie"
     },
-    control_delete_this_clone: {
-        blockcode: 'verwijder deze kloon'
+    "motion_yposition": {
+        "blockcode": "y-positie"
     },
-    motion_xposition: {
-        blockcode: 'x-positie'
+    "motion_direction": {
+        "blockcode": "richting"
     },
-    motion_yposition: {
-        blockcode: 'y-positie'
+    "looks_costumenumbername": {
+        "blockcode": "uiterlijk %m ::looks"
     },
-    motion_direction: {
-        blockcode: 'richting'
+    "looks_backdropnumbername": {
+        "blockcode": "achtergrond %m ::looks"
     },
-    looks_costumenumbername: {
-        blockcode: 'uiterlijk %m ::looks'
+    "looks_size": {
+        "blockcode": "grootte"
     },
-    looks_backdropnumbername: {
-        blockcode: 'achtergrond %m ::looks'
+    "sound_volume": {
+        "blockcode": "volume"
     },
-    looks_size: {
-        blockcode: 'grootte'
+    "sensing_touchingobject": {
+        "blockcode": "raakt ik %r ?"
     },
-    sound_volume: {
-        blockcode: 'volume'
+    "sensing_touchingobjectmenu": {
+        "blockcode": ""
     },
-    sensing_touchingobject: {
-        blockcode: 'raakt ik %r ?'
+    "sensing_touchingcolor": {
+        "blockcode": "raak ik kleur %c ?"
     },
-    sensing_touchingobjectmenu: {
-        blockcode: ''
+    "sensing_coloristouchingcolor": {
+        "blockcode": "raakt kleur %c kleur %c ?"
     },
-    sensing_touchingcolor: {
-        blockcode: 'raak ik kleur %c ?'
+    "sensing_distanceto": {
+        "blockcode": "afstand tot %r"
     },
-    sensing_coloristouchingcolor: {
-        blockcode: 'raakt kleur %c kleur %c ?'
+    "sensing_distancetomenu": {
+        "blockcode": ""
     },
-    sensing_distanceto: {
-        blockcode: 'afstand tot %r'
+    "sensing_askandwait": {
+        "blockcode": "vraag %s en wacht"
     },
-    sensing_distancetomenu: {
-        blockcode: ''
+    "sensing_answer": {
+        "blockcode": "antwoord"
     },
-    sensing_askandwait: {
-        blockcode: 'vraag %s en wacht'
+    "sensing_keypressed": {
+        "blockcode": "toets %r ingedrukt?"
     },
-    sensing_answer: {
-        blockcode: 'antwoord'
+    "sensing_keyoptions": {
+        "blockcode": "%m"
     },
-    sensing_keypressed: {
-        blockcode: 'toets %r ingedrukt?'
+    "sensing_mousedown": {
+        "blockcode": "muis ingedrukt?"
     },
-    sensing_keyoptions: {
-        blockcode: '%m'
+    "sensing_mousex": {
+        "blockcode": "muis x"
     },
-    sensing_mousedown: {
-        blockcode: 'muis ingedrukt?'
+    "sensing_mousey": {
+        "blockcode": "muis y"
     },
-    sensing_mousex: {
-        blockcode: 'muis x'
+    "sensing_setdragmode": {
+        "blockcode": "zet sleepbaar op %m"
     },
-    sensing_mousey: {
-        blockcode: 'muis y'
+    "sensing_loudness": {
+        "blockcode": "volume"
     },
-    sensing_setdragmode: {
-        blockcode: 'zet sleepbaar op %m'
+    "sensing_timer": {
+        "blockcode": "klok"
     },
-    sensing_loudness: {
-        blockcode: 'volume'
+    "sensing_resettimer": {
+        "blockcode": "zet klok op 0"
     },
-    sensing_timer: {
-        blockcode: 'klok'
+    "sensing_of": {
+        "blockcode": "%m van %r ::sensing"
     },
-    sensing_resettimer: {
-        blockcode: 'zet klok op 0'
+    "sensing_of_object_menu": {
+        "blockcode": ""
     },
-    sensing_of: {
-        blockcode: '%m van %r ::sensing'
+    "sensing_current": {
+        "blockcode": "huidige %m"
     },
-    sensing_of_object_menu: {
-        blockcode: ''
+    "sensing_dayssince2000": {
+        "blockcode": "dagen sinds 2000"
     },
-    sensing_current: {
-        blockcode: 'huidige %m'
+    "sensing_username": {
+        "blockcode": "gebruikersnaam"
     },
-    sensing_dayssince2000: {
-        blockcode: 'dagen sinds 2000'
+    "operator_add": {
+        "blockcode": "%n + %n"
     },
-    sensing_username: {
-        blockcode: 'gebruikersnaam'
+    "operator_subtract": {
+        "blockcode": "%n - %n"
     },
-    operator_add: {
-        blockcode: '%n + %n'
+    "operator_multiply": {
+        "blockcode": "%n * %n"
     },
-    operator_subtract: {
-        blockcode: '%n - %n'
+    "operator_divide": {
+        "blockcode": "%n / %n"
     },
-    operator_multiply: {
-        blockcode: '%n * %n'
+    "operator_random": {
+        "blockcode": "willekeurig getal tussen %n en %n"
     },
-    operator_divide: {
-        blockcode: '%n / %n'
+    "operator_gt": {
+        "blockcode": "%n > %n"
     },
-    operator_random: {
-        blockcode: 'willekeurig getal tussen %n en %n'
+    "operator_lt": {
+        "blockcode": "%n < %n"
     },
-    operator_gt: {
-        blockcode: '%n > %n'
+    "operator_equals": {
+        "blockcode": "%n = %n"
     },
-    operator_lt: {
-        blockcode: '%n < %n'
+    "operator_and": {
+        "blockcode": "%b and %b"
     },
-    operator_equals: {
-        blockcode: '%n = %n'
+    "operator_or": {
+        "blockcode": "%b or %b"
     },
-    operator_and: {
-        blockcode: '%b and %b'
+    "operator_not": {
+        "blockcode": "niet %b"
     },
-    operator_or: {
-        blockcode: '%b or %b'
+    "operator_join": {
+        "blockcode": "voeg %s en %s samen"
     },
-    operator_not: {
-        blockcode: 'niet %b'
+    "operator_letter_of": {
+        "blockcode": "letter %n van %s"
     },
-    operator_join: {
-        blockcode: 'voeg %s en %s samen'
+    "operator_length": {
+        "blockcode": "lengte van %s"
     },
-    operator_letter_of: {
-        blockcode: 'letter %n van %s'
+    "operator_contains": {
+        "blockcode": "%s bevat %s ? "
     },
-    operator_length: {
-        blockcode: 'lengte van %s'
+    "operator_mod": {
+        "blockcode": "%n modulo %n"
     },
-    operator_contains: {
-        blockcode: '%s bevat %s ? '
+    "operator_round": {
+        "blockcode": "afgerond %n"
     },
-    operator_mod: {
-        blockcode: '%n modulo %n'
+    "operator_mathop": {
+        "blockcode": "%m van %n ::operators"
     },
-    operator_round: {
-        blockcode: 'afgerond %n'
+    "data_setvariableto": {
+        "blockcode": "maak %m %s"
     },
-    operator_mathop: {
-        blockcode: '%m van %n ::operators'
+    "data_changevariableby": {
+        "blockcode": "verander %m met %n"
     },
-    undefined: {
-        blockcode: 'undefined'
+    "data_showvariable": {
+        "blockcode": "toon variabele %m"
     },
-    data_setvariableto: {
-        blockcode: 'maak %m %s'
+    "data_hidevariable": {
+        "blockcode": "verberg variabele %m"
     },
-    data_changevariableby: {
-        blockcode: 'verander %m met %n'
+    "undefined": {
+        "blockcode": "undefined"
     },
-    data_showvariable: {
-        blockcode: 'toon variabele %m'
+    "data_addtolist": {
+        "blockcode": "voeg %s toe aan %m"
     },
-    data_hidevariable: {
-        blockcode: 'verberg variabele %m'
+    "data_deleteoflist": {
+        "blockcode": "verwijder %n van %m"
     },
-    undefined: {
-        blockcode: 'undefined'
+    "data_deletealloflist": {
+        "blockcode": "verwijder alle van %m"
     },
-    data_addtolist: {
-        blockcode: 'voeg %s toe aan %m'
+    "data_insertatlist": {
+        "blockcode": "voeg %s toe op %n van %m"
     },
-    data_deleteoflist: {
-        blockcode: 'verwijder %n van %m'
+    "data_replaceitemoflist": {
+        "blockcode": "vervang item %n van %m door %s"
     },
-    data_deletealloflist: {
-        blockcode: 'verwijder alle van %m'
+    "data_itemoflist": {
+        "blockcode": "item %n van %m"
     },
-    data_insertatlist: {
-        blockcode: 'voeg %s toe op %n van %m'
+    "data_itemnumoflist": {
+        "blockcode": "item # van %s in %m"
     },
-    data_replaceitemoflist: {
-        blockcode: 'vervang item %n van %m door %s'
+    "data_lengthoflist": {
+        "blockcode": "lengte van %m"
     },
-    data_itemoflist: {
-        blockcode: 'item %n van %m'
+    "data_listcontainsitem": {
+        "blockcode": "%m bevat %s ? ::lists"
     },
-    data_itemnumoflist: {
-        blockcode: 'item # van %s in %m'
+    "data_showlist": {
+        "blockcode": "toon lijst %m"
     },
-    data_lengthoflist: {
-        blockcode: 'lengte van %m'
-    },
-    data_listcontainsitem: {
-        blockcode: '%m bevat %s ? ::lists'
-    },
-    data_showlist: {
-        blockcode: 'toon lijst %m'
-    },
-    data_hidelist: {
-        blockcode: 'verberg lijst %m'
-    },
+    "data_hidelist": {
+        "blockcode": "verberg lijst %m"
+    }
 }
